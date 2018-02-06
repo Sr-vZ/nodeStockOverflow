d87c3c8dc1afb5685661b50aa287bfd4e67558a6
Sourav De <srvz39@gmail.com>
2018-02-06 18:19:23 +0530

    addedautocomplete for the tickers

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const app = require('express')()
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
 
 // set the view engine to ejs
 app.set('view engine', 'ejs')
 
+// set the static resources (css/js) to public folder
+app.use('/public',express.static(path.join(__dirname, 'public')));
+
 // blog home page
 app.get('/', (req, res) => {
     // render `home.ejs` with the list of posts
-    res.render('home', { })
+    data = fetchTickers()
+    res.render('home', {tickers: data })
 })
 
 // blog post
@@ -23,7 +30,22 @@ app.get('/', (req, res) => {
         body: post.body
     })
 }) */
-
+app.get('/tickerlist',(req,res)=>{
+    var NSEtickers = [];
+    NSEtickers = fetchTickers();
+    res.json(NSEtickers)
+})
 app.listen(8180)
 
 console.log('listening on port 8180')
+
+function fetchTickers() {
+    var NSEtickers = [];
+    var fileContents = fs.readFileSync('./data/NSE-datasets-codes.csv');
+    var lines = fileContents.toString().split('\n');
+
+    for (var i = 0; i < lines.length; i++) {
+        NSEtickers.push(lines[i].toString().split(','));
+    }
+    return NSEtickers;
+}
